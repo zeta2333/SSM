e7cedcc07e6949de2cc1aa4570d6b17a363e021e
Pycro <2023747015@qq.com>
2023-03-22 16:06:36 +0800

    3.22 update

--- spring-transaction/src/test/java/usts/pycro/spring/test/JdbcTemplateTest.java
+++ spring-transaction/src/test/java/usts/pycro/spring/test/JdbcTemplateTest.java
@@ -3,9 +3,13 @@ package usts.pycro.spring.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import usts.pycro.spring.pojo.User;
+
+import java.util.List;
 
 /**
  * @author Pycro
@@ -13,6 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * 2023-03-22 15:40
  */
 //指定当前测试类在spring的测试环境中执行，此时就可以通过注入的方式直接获取IOC容器中的bean
+// 相当于给当前的测试类赋予了bean的功能
 @RunWith(SpringJUnit4ClassRunner.class)
 //设置Spring测试环境的配置文件
 @ContextConfiguration("classpath:spring-jdbc.xml")
@@ -23,7 +28,28 @@ public class JdbcTemplateTest {
     @Test
     public void testJdbcTemplate() {
         String sql = "insert into t_user values(null,?,?,?,?,?)";
-        int result = jdbcTemplate.update(sql, "root","123",22,"女","213@qq.com");
+        int result = jdbcTemplate.update(sql, "root", "123", 22, "女", "213@qq.com");
         System.out.println(result);
+    }
+
+    @Test
+    public void testGetUserById() {
+        String sql = "select * from t_user where id = ?";
+        User user = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(User.class), 1);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testGetAllUser() {
+        String sql = "select * from t_user";
+        List<User> userList = jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(User.class));
+        userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void testGetCount() {
+        String sql = "select count(*) from t_user";
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
+        System.out.println(count);
     }
 }
